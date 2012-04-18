af218ac7df3ac44678dc4a3edbf5df4b056179b6
y.kamata <y-kamata@lucent-global.co.jp>
2012-04-18 10:08:04 -0400

    first commit

---
+++ data/Smarty/templates_c/default/%%2C^2CF^2CF22D19%%header.tpl.php
@@ -0,0 +1,70 @@
+<?php /* Smarty version 2.6.26, created on 2012-04-18 03:52:59
+         compiled from /home/y-kamata/y-kamata.dev.1ban-shop.com/html/../data/Smarty/templates/default/header.tpl */ ?>
+<?php require_once(SMARTY_CORE_DIR . 'core.load_plugins.php');
+smarty_core_load_plugins(array('plugins' => array(array('modifier', 'script_escape', '/home/y-kamata/y-kamata.dev.1ban-shop.com/html/../data/Smarty/templates/default/header.tpl', 28, false),array('modifier', 'h', '/home/y-kamata/y-kamata.dev.1ban-shop.com/html/../data/Smarty/templates/default/header.tpl', 28, false),array('modifier', 'count', '/home/y-kamata/y-kamata.dev.1ban-shop.com/html/../data/Smarty/templates/default/header.tpl', 34, false),)), $this); ?>
+<!--▼HEADER-->
+<div id="header_wrap">
+    <div id="header" class="clearfix">
+        <div id="logo_area">
+            <p id="site_description">EC-CUBE発!世界中を旅して見つけた立方体グルメを立方隊長が直送！</p>
+            <h1>
+                <a href="<?php echo ((is_array($_tmp=@TOP_URLPATH)) ? $this->_run_mod_handler('script_escape', true, $_tmp) : smarty_modifier_script_escape($_tmp)); ?>
+"><img src="<?php echo ((is_array($_tmp=$this->_tpl_vars['TPL_URLPATH'])) ? $this->_run_mod_handler('script_escape', true, $_tmp) : smarty_modifier_script_escape($_tmp)); ?>
+img/common/logo.gif" alt="EC-CUBE ONLINE SHOPPING SITE" /><span><?php echo ((is_array($_tmp=((is_array($_tmp=$this->_tpl_vars['arrSiteInfo']['shop_name'])) ? $this->_run_mod_handler('script_escape', true, $_tmp) : smarty_modifier_script_escape($_tmp)))) ? $this->_run_mod_handler('h', true, $_tmp) : smarty_modifier_h($_tmp)); ?>
+/<?php echo ((is_array($_tmp=((is_array($_tmp=$this->_tpl_vars['tpl_title'])) ? $this->_run_mod_handler('script_escape', true, $_tmp) : smarty_modifier_script_escape($_tmp)))) ? $this->_run_mod_handler('h', true, $_tmp) : smarty_modifier_h($_tmp)); ?>
+</span></a>
+            </h1>
+        </div>
+        <div id="header_utility">
+            <div id="headerInternalColumn">
+                        <?php if (count(((is_array($_tmp=$this->_tpl_vars['arrPageLayout']['HeaderInternalNavi'])) ? $this->_run_mod_handler('script_escape', true, $_tmp) : smarty_modifier_script_escape($_tmp))) > 0): ?>
+                                <?php $_from = ((is_array($_tmp=$this->_tpl_vars['arrPageLayout']['HeaderInternalNavi'])) ? $this->_run_mod_handler('script_escape', true, $_tmp) : smarty_modifier_script_escape($_tmp)); if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array'); }if (count($_from)):
+    foreach ($_from as $this->_tpl_vars['HeaderInternalNaviKey'] => $this->_tpl_vars['HeaderInternalNaviItem']):
+?>
+                    <!-- ▼<?php echo ((is_array($_tmp=$this->_tpl_vars['HeaderInternalNaviItem']['bloc_name'])) ? $this->_run_mod_handler('script_escape', true, $_tmp) : smarty_modifier_script_escape($_tmp)); ?>
+ -->
+                    <?php if (((is_array($_tmp=$this->_tpl_vars['HeaderInternalNaviItem']['php_path'])) ? $this->_run_mod_handler('script_escape', true, $_tmp) : smarty_modifier_script_escape($_tmp)) != ""): ?>
+                        <?php require_once(SMARTY_CORE_DIR . 'core.smarty_include_php.php');
+smarty_core_smarty_include_php(array('smarty_file' => ((is_array($_tmp=$this->_tpl_vars['HeaderInternalNaviItem']['php_path'])) ? $this->_run_mod_handler('script_escape', true, $_tmp) : smarty_modifier_script_escape($_tmp)), 'smarty_assign' => '', 'smarty_once' => false, 'smarty_include_vars' => array('items' => ((is_array($_tmp=$this->_tpl_vars['HeaderInternalNaviItem'])) ? $this->_run_mod_handler('script_escape', true, $_tmp) : smarty_modifier_script_escape($_tmp)))), $this); ?>
+
+                    <?php else: ?>
+                        <?php $_smarty_tpl_vars = $this->_tpl_vars;
+$this->_smarty_include(array('smarty_include_tpl_file' => ((is_array($_tmp=$this->_tpl_vars['HeaderInternalNaviItem']['tpl_path'])) ? $this->_run_mod_handler('script_escape', true, $_tmp) : smarty_modifier_script_escape($_tmp)), 'smarty_include_vars' => array('items' => ((is_array($_tmp=$this->_tpl_vars['HeaderInternalNaviItem'])) ? $this->_run_mod_handler('script_escape', true, $_tmp) : smarty_modifier_script_escape($_tmp)))));
+$this->_tpl_vars = $_smarty_tpl_vars;
+unset($_smarty_tpl_vars);
+ ?>
+                    <?php endif; ?>
+                    <!-- ▲<?php echo ((is_array($_tmp=$this->_tpl_vars['HeaderInternalNaviItem']['bloc_name'])) ? $this->_run_mod_handler('script_escape', true, $_tmp) : smarty_modifier_script_escape($_tmp)); ?>
+ -->
+                <?php endforeach; endif; unset($_from); ?>
+                            <?php endif; ?>
+                        </div>
+            <div id="header_navi">
+                <ul>
+                    <li class="mypage">
+                        <a href="<?php echo ((is_array($_tmp=@HTTPS_URL)) ? $this->_run_mod_handler('script_escape', true, $_tmp) : smarty_modifier_script_escape($_tmp)); ?>
+mypage/login.php" onmouseover="chgImg('<?php echo ((is_array($_tmp=$this->_tpl_vars['TPL_URLPATH'])) ? $this->_run_mod_handler('script_escape', true, $_tmp) : smarty_modifier_script_escape($_tmp)); ?>
+img/common/btn_header_mypage_on.jpg','mypage');" onmouseout="chgImg('<?php echo ((is_array($_tmp=$this->_tpl_vars['TPL_URLPATH'])) ? $this->_run_mod_handler('script_escape', true, $_tmp) : smarty_modifier_script_escape($_tmp)); ?>
+img/common/btn_header_mypage.jpg','mypage');"><img src="<?php echo ((is_array($_tmp=$this->_tpl_vars['TPL_URLPATH'])) ? $this->_run_mod_handler('script_escape', true, $_tmp) : smarty_modifier_script_escape($_tmp)); ?>
+img/common/btn_header_mypage.jpg" alt="MYページ" name="mypage" id="mypage" /></a>
+                    </li>
+                    <li class="entry">
+                        <a href="<?php echo ((is_array($_tmp=@ROOT_URLPATH)) ? $this->_run_mod_handler('script_escape', true, $_tmp) : smarty_modifier_script_escape($_tmp)); ?>
+entry/kiyaku.php" onmouseover="chgImg('<?php echo ((is_array($_tmp=$this->_tpl_vars['TPL_URLPATH'])) ? $this->_run_mod_handler('script_escape', true, $_tmp) : smarty_modifier_script_escape($_tmp)); ?>
+img/common/btn_header_entry_on.jpg','entry');" onmouseout="chgImg('<?php echo ((is_array($_tmp=$this->_tpl_vars['TPL_URLPATH'])) ? $this->_run_mod_handler('script_escape', true, $_tmp) : smarty_modifier_script_escape($_tmp)); ?>
+img/common/btn_header_entry.jpg','entry');"><img src="<?php echo ((is_array($_tmp=$this->_tpl_vars['TPL_URLPATH'])) ? $this->_run_mod_handler('script_escape', true, $_tmp) : smarty_modifier_script_escape($_tmp)); ?>
+img/common/btn_header_entry.jpg" alt="会員登録" name="entry" id="entry" /></a>
+                    </li>
+                    <li>
+                        <a href="<?php echo ((is_array($_tmp=@CART_URLPATH)) ? $this->_run_mod_handler('script_escape', true, $_tmp) : smarty_modifier_script_escape($_tmp)); ?>
+" onmouseover="chgImg('<?php echo ((is_array($_tmp=$this->_tpl_vars['TPL_URLPATH'])) ? $this->_run_mod_handler('script_escape', true, $_tmp) : smarty_modifier_script_escape($_tmp)); ?>
+img/common/btn_header_cart_on.jpg','cartin');" onmouseout="chgImg('<?php echo ((is_array($_tmp=$this->_tpl_vars['TPL_URLPATH'])) ? $this->_run_mod_handler('script_escape', true, $_tmp) : smarty_modifier_script_escape($_tmp)); ?>
+img/common/btn_header_cart.jpg','cartin');"><img src="<?php echo ((is_array($_tmp=$this->_tpl_vars['TPL_URLPATH'])) ? $this->_run_mod_handler('script_escape', true, $_tmp) : smarty_modifier_script_escape($_tmp)); ?>
+img/common/btn_header_cart.jpg" alt="カゴの中を見る" name="cartin" id="cartin" /></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<!--▲HEADER-->
---
+++ data/config/config.php
@@ -0,0 +1,18 @@
+<?php
+    define ('ECCUBE_INSTALL', 'ON');
+    define ('HTTP_URL', 'http://y-kamata.dev.1ban-shop.com/');
+    define ('HTTPS_URL', 'http://y-kamata.dev.1ban-shop.com/');
+    define ('ROOT_URLPATH', '/');
+    define ('DOMAIN_NAME', '');
+    define ('DB_TYPE', 'pgsql');
+    define ('DB_USER', 'lucent08');
+    define ('DB_PASSWORD', 'LkgtIY00Dhinxc2KhEh4');
+    define ('DB_SERVER', '127.0.0.1');
+    define ('DB_NAME', 'itiban');
+    define ('DB_PORT', '5432');
+    define ('ADMIN_DIR', 'admin/');
+    define ('ADMIN_FORCE_SSL', FALSE);
+    define ('ADMIN_ALLOW_HOSTS', 'a:0:{}');
+    define ('AUTH_MAGIC', 'fracialaciakocrohouhiodudeslesledaerathi');
+    define ('PASSWORD_HASH_ALGOS', 'sha256');
+?>
